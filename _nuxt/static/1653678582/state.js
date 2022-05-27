21a97bcd6a3d8ab92ce8482f611dd64710caf92a
david istomin <daveistomin@gmail.com>
2022-05-27 23:15:07 +0400

    2e74793

---
+++ _nuxt/static/1653678582/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcapella_landing\u002F_nuxt\u002Fstatic\u002F1653678582",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fcapella_landing\u002F",assetsPath:"\u002Fcapella_landing\u002F_nuxt\u002F",cdnURL:a}}}}(null));
